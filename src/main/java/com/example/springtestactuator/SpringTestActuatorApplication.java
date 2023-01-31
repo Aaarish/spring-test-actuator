6d3c6a4e6145aada2b24a7f102ea0279b5ffb687
Aarish <aarishm767@gmail.com>
2023-01-31 14:27:52 +0530

    Initial Commit

---
+++ src/main/java/com/example/springtestactuator/SpringTestActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.springtestactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTestActuatorApplication.class, args);
+	}
+
+}
